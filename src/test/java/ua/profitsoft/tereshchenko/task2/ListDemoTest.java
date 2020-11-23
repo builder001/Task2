a882ce8d088f06cd1c6dce346fb578ef4880e7d4
Anton Tereshchenko <sourcein101@gmail.com>
2020-11-23 14:04:45 +0200

    NullPointerException test added.

--- src/test/java/ua/profitsoft/tereshchenko/task2/ListDemoTest.java
+++ src/test/java/ua/profitsoft/tereshchenko/task2/ListDemoTest.java
@@ -1,10 +1,13 @@
 package ua.profitsoft.tereshchenko.task2;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ListDemoTest {
     @Test
@@ -13,10 +16,24 @@ public class ListDemoTest {
         List<String> list2 = Arrays.asList("a", "c", "b");
         assertEquals(true, ListDemo.compareTwoLists(list1, list2));
     }
+
     @Test
     public void compareTwoListsTest2() {
         List<String> list1 = Arrays.asList("a", "b", "c");
         List<String> list2 = Arrays.asList("b", "b", "c");
         assertEquals(false, ListDemo.compareTwoLists(list1, list2));
+    }
+
+    @Test
+    public void compareTwoListsTestNPEThrow() {
+        List<String> list1 = Arrays.asList("a", "b", "c");
+        List<String> list2 = null;
+        assertThrows(NullPointerException.class,
+                new Executable() {
+                    @Override
+                    public void execute() throws Throwable {
+                        ListDemo.compareTwoLists(list1, list2);
+                    }
+                });
     }
 }
